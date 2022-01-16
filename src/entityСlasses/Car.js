7c1ae6071e3442bfb22d962159cba4d2a82081ab
xdpiqbx <xdpiqbx@gmail.com>
2022-01-16 22:37:34 +0200

    inital commit

---
+++ src/entityСlasses/Car.js
@@ -0,0 +1,8 @@
+// const car = {
+//   _id: null,
+//   model: '',
+//   number: '',
+//   gasoline_residue: 0,
+//   driversIds: [],
+//   refuelStatCarId: null
+// };
---
+++ src/entityСlasses/Driver.js
@@ -0,0 +1,10 @@
+// const driver = {
+//   _id: null,
+//   name: '',
+//   status: 5,
+//   carsIds: [],
+//   tlg_chatId: null,
+//   candidateChatId: null,
+//   giveOutOrRefue: false,
+//   creatorChatId: 938358368
+// };
